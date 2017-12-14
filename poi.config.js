66aa9214493a29349090e886de4133b788fb8e38
fireyy <fireyy@gmail.com>
2017-12-14 17:40:54 +0800

    fix: github pages

--- poi.config.js
+++ poi.config.js
@@ -3,7 +3,7 @@ const path = require("path")
 module.exports = {
   entry: "example/index.js",
   dist: "example/dist",
-  homepage: "/",
+  homepage: "",
   presets: [
     require("poi-preset-sass-resources")({
       resources: path.join(__dirname, "src", "_variables.scss")
