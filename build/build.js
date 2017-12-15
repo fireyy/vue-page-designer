aceb55c643ce19b68c3fd69519d739e0f475a43b
fireyy <fireyy@gmail.com>
2017-12-15 16:53:05 +0800

    feat: add release script

--- build/build.js
+++ build/build.js
@@ -34,9 +34,5 @@ rm(assetsRoot, err => {
     }
 
     console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
-    ))
   })
 })
