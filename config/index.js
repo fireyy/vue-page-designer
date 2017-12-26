e30f5bfe176935f0da10002d0820a9c78a052752
fireyy <fireyy@gmail.com>
2017-12-26 16:23:00 +0800

    fix: example static path

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../example/dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
